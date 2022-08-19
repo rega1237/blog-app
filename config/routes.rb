47b312050ccb04c14279ff725f7dd8e4f807dc09
Rafael Guzman <rega1237@gmail.com>
2022-08-19 08:49:35 -0400

    Add the forms for new post, new comment and add like using current user.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   #root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show] do
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
+    end
   end
+
+  resources :posts, only: [:new, :create]
 end
